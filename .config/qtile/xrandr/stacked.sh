df329766fc64cf91f9d195111e6c3051c98bbdad
David Wagner <dwagner86@gmail.com>
2021-07-23 16:52:31 -0400

    adds an xrandr layout for laptop

--- .config/qtile/xrandr/stacked.sh
+++ .config/qtile/xrandr/stacked.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "setting layout"
+echo "setting layout: stacked"
 xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-4 --primary --mode 2560x1440 --pos 0x1440 --rotate normal --output DP-5 --off
