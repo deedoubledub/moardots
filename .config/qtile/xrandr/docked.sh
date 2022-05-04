9b795a3517577f6a14f8e29c6424748e4d9dbd02
David Wagner <dwagner86@gmail.com>
2022-05-04 09:55:37 -0400

    updates screen layouts

--- .config/qtile/xrandr/docked.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr -d :0 --output eDP-1 --mode 1366x768 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --primary --mode 1920x1080 --pos 1366x1080 --rotate normal --output DP-2-2 --mode 1920x1080 --pos 1366x0 --rotate normal --output DP-2-3 --off
--- .config/qtile/xrandr/framedocked.sh
+++ .config/qtile/xrandr/framedocked.sh
@@ -4,14 +4,6 @@
 xrandr \
   -d :0 \
   --fb 5136x3240 \
-  --output eDP-1 --mode 2256x1504 --pos 0x1736 \
-  --output DP-4 --mode 1920x1080 --pos 2256x0 --scale 1.5x1.5 \
-  --output DP-3-1 --primary --mode 1920x1080 --pos 2256x1620 --scale 1.5x1.5
-
-## 2x scaling
-# xrandr \
-#   -d :0 \
-#   --fb 6096x4320 \
-#   --output eDP-1 --mode 2256x1504 --pos 0x2816 \
-#   --output DP-4 --mode 1920x1080 --pos 2256x0 --scale 2x2 \
-#   --output DP-3-1 --primary --mode 1920x1080 --pos 2256x2160 --scale 2x2
+  --output eDP-1 --mode 2256x1504 --pos 2880x1736 \
+  --output DP-4 --mode 1920x1080 --pos 0x0 --scale 1.5x1.5 \
+  --output DP-3-1 --primary --mode 1920x1080 --pos 0x1620 --scale 1.5x1.5
