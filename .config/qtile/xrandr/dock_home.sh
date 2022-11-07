c57c6ebfa9527e5eef977fa690f70a11fd45ab3f
David Wagner <dwagner86@gmail.com>
2022-11-07 11:54:11 -0500

    improves scaling for home dock

--- .config/qtile/xrandr/dock_home.sh
+++ .config/qtile/xrandr/dock_home.sh
@@ -2,6 +2,7 @@
 
 xrandr \
   -d :0 \
-  --output eDP-1 --mode 2256x1504 --pos 2560x1376 \
-  --output DP-1 --primary --mode 2560x1440 --pos 0x1440 \
-  --output DP-2-2-1 --mode 2560x1440 --pos 0x0
+  --fb 5328x3456 \
+  --output eDP-1 --mode 2256x1504 --pos 3072x1952 \
+  --output DP-2-2-1 --mode 2560x1440 --pos 0x0 --scale 1.2x1.2 \
+  --output DP-1 --primary --mode 2560x1440 --pos 0x1728 --scale 1.2x1.2
